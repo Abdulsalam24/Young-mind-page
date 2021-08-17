2050115891a608364125e5fe016d4e4791c2d4f3
Abdulsalam24 <78443975+Abdulsalam24@users.noreply.github.com>
2021-08-17 21:02:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let nav =  document.querySelector(".none");
+
+    if(nav.style.display === "block"){
+        nav.style.display = "none";
+    }else{
+        nav.style.display = "block";
+    }
+}
+
